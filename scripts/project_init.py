73c8a84e3901e7f96f3b4b590fc41dd497f45e80
Sid Prabhakaran <s2siddhu@gmail.com>
2023-11-02 18:34:00 -0500

    changed project init replace to project-name from project_name

--- scripts/project_init.py
+++ scripts/project_init.py
@@ -2,15 +2,11 @@ import os
 import argparse
 import fileinput
 
-# Write a script that copies the project.xml and Sconstruct file from project-init-templates into the current directory and replaces the project name with the name of the current directory.
 def main():
     # Get project name from CLI argument
     parser = argparse.ArgumentParser("Initialize a new Embr project.")
     parser.add_argument("project_name", help="Name of the project to initialize.")
     args = parser.parse_args()
-
-    # Print current directory name
-    # print(f"Current directory name: {os.getcwd()}")
 
     # Create a folder named project-name-src if it doesn't already exist
     project_name = args.project_name
@@ -22,13 +18,13 @@ def main():
 
     # Replace project name in project.xml and Sconstruct with project-name
     for line in fileinput.input(f"{project_name}-src/project.xml", inplace=True):
-        print(line.replace("project_name", project_name), end="")
+        print(line.replace("project-name", project_name), end="")
 
     for line in fileinput.input(f"{project_name}-src/Sconstruct", inplace=True):
-        print(line.replace("project_name", project_name), end="")
+        print(line.replace("project-name", project_name), end="")
     
     # Print a message to the user
-    print(f"Initialized project {project_name}.")
+    print(f"\nInitialized project {project_name}.")
 
 if __name__ == "__main__":
     main()
